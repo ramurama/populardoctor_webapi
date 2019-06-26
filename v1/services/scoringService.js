0242406f99bb6e477823703ca5b2ea48e0f22a14
Ramu Ramasamy <rram.ramasamy@gmail.com>
2019-06-26 14:04:38 +0530

    scoring engine complete

--- v1/services/scoringService.js
+++ v1/services/scoringService.js
@@ -6,6 +6,7 @@ const Scores = mongoose.model(modelNames.SCORES);
 const config = require('../../config/scoring.json');
 const _ = require('underscore');
 const moment = require('moment');
+const flat = require('flat');
 
 const BOOKING_START_TIME_LIMIT = require('../../config/booking.json')
   .bookingStartLimit;
@@ -69,7 +70,7 @@ module.exports = {
           const trust = userTrustScores.reduce(
             (accumulatedTrust, currentTrust) => accumulatedTrust + currentTrust
           );
-
+          console.log('Trust -- ' + trust);
           //****************************************
           // popularity scores computation per doctor
           const popularityScores = drbookings.bookings.map(booking =>
@@ -79,41 +80,51 @@ module.exports = {
             (accumulatedPopularity, currentPopularity) =>
               accumulatedPopularity + currentPopularity
           );
-
+          console.log('Popularity -- ' + popularity);
           //****************************************
           // schedule booking speed computation per doctor
           //grouping by tokendate
           const tokenDateGrouped = _.groupBy(drbookings.bookings, 'tokenDate');
-          Object.values(tokenDateGrouped).map(booking => {
-            //grouping by schedule for a particular token date
-            const scheduleGrouped = _.groupBy(booking, 'scheduleId');
-            const scheduleBookingRates = Object.values(scheduleGrouped).map(
-              bookings => {
-                //bookings beloging to a particular schedule in a particular token date
-                const bookingRates = bookings.map(booking => {
-                  //compare booking open time (start time - booking limit) and booked time
-                  // find the difference in seconds and return as array
-                  const startTimeMoment = moment(
-                    booking.startTimeStamp
-                  ).subtract(BOOKING_START_TIME_LIMIT, 'hours');
-                  const bookedTimeMoment = moment(booking.bookedTimeStamp);
-                  return moment
-                    .duration(bookedTimeMoment.diff(startTimeMoment))
-                    .asSeconds();
-                });
-                //compute the average booking rate for all the bookings made for a particular schedule in  a particular token date
-                const length = bookingRates.length;
-                const sumBookingRates = bookingRates.reduce(
-                  (accumulatedValue, currentValue) =>
-                    accumulatedValue + currentValue
-                );
-                //return the average
-                return Math.round(sumBookingRates / length);
-              }
-            );
-            //compute scores for each of the average booking rate
-            console.log(scheduleBookingRates);
-          });
+          let scheduleGroupedRates = Object.values(tokenDateGrouped).map(
+            booking => {
+              //grouping by schedule for a particular token date
+              const scheduleGrouped = _.groupBy(booking, 'scheduleId');
+              const scheduleBookingRates = Object.values(scheduleGrouped).map(
+                bookings => {
+                  //bookings beloging to a particular schedule in a particular token date
+                  const bookingRates = bookings.map(booking => {
+                    //compare booking open time (start time - booking limit) and booked time
+                    // find the difference in seconds and return as array
+                    const startTimeMoment = moment(
+                      booking.startTimeStamp
+                    ).subtract(BOOKING_START_TIME_LIMIT, 'hours');
+                    const bookedTimeMoment = moment(booking.bookedTimeStamp);
+                    return moment
+                      .duration(bookedTimeMoment.diff(startTimeMoment))
+                      .asSeconds();
+                  });
+                  //compute the average booking rate for all the bookings made for a particular schedule in  a particular token date
+                  const length = bookingRates.length;
+                  const sumBookingRates = bookingRates.reduce(
+                    (accumulatedValue, currentValue) =>
+                      accumulatedValue + currentValue
+                  );
+                  //return the average
+                  return Math.round(sumBookingRates / length);
+                }
+              );
+              return scheduleBookingRates;
+            }
+          );
+          scheduleGroupedRates = Object.values(flat(scheduleGroupedRates));
+          const scheduleScores = scheduleGroupedRates.map(rate =>
+            _computeScheduleScore(rate)
+          );
+          const schedule = scheduleScores.reduce(
+            (accumulatedScheduleScore, currentScheduleScore) =>
+              accumulatedScheduleScore + currentScheduleScore
+          );
+          console.log('Schedule -- ' + schedule);
         });
       }
     );
@@ -186,7 +197,6 @@ function _computePopularity(bookingDistance) {
   const { d1, d2, d3, d4 } = distance;
   const { p1, p2, p3, p4 } = points;
   let popularity = 0;
-
   if (bookingDistance > d1 && bookingDistance <= d2) {
     popularity = p1;
   } else if (bookingDistance > d2 && bookingDistance <= d3) {
@@ -196,8 +206,26 @@ function _computePopularity(bookingDistance) {
   } else if (bookingDistance > d4) {
     popularity = p4;
   }
-
   return popularity;
+}
+
+function _computeScheduleScore(rate) {
+  const { time, points } = config.schedule;
+  const { t1, t2, t3, t4 } = time;
+  const { p1, p2, p3, p4, p5 } = points;
+  let scheduleScore = 0;
+  if (rate <= t1) {
+    scheduleScore = p1;
+  } else if (rate > t1 && rate <= t2) {
+    scheduleScore = p2;
+  } else if (rate > t2 && rate <= t3) {
+    scheduleScore = p3;
+  } else if (rate > t3 && rate <= t4) {
+    scheduleScore = p4;
+  } else {
+    scheduleScore = p5;
+  }
+  return scheduleScore;
 }
 
 // function _computeTrust(count) {
